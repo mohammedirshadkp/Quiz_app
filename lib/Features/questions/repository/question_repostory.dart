66472bc94d52c5289ef427255fbe7d1c1d5af307
irshad500 <mohammedirshadkp500@gmail.com>
2024-01-31 15:28:01 +0530

    bug resolved

--- lib/Features/questions/repository/question_repostory.dart
+++ lib/Features/questions/repository/question_repostory.dart
@@ -26,7 +26,7 @@ class QuizRepository {
   }
 
   Future<void> saveDataLocally(List<dynamic> data) async {
-    final box = await Hive.openBox('daabox');
+    final box = await Hive.openBox('daaox');
     await box.put('dataKey', data);
     await box.close();
   }
