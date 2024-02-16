c3e8b4db1c560739616ccf81fc685d84a023d3ec
irshad500 <mohammedirshadkp500@gmail.com>
2024-02-16 15:04:26 +0530

    bug resolved

--- lib/Features/questions/controller/question_controller.dart
+++ lib/Features/questions/controller/question_controller.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:fpdart/src/either.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:quiz_app/Core/faliure.dart';
 
 import '../repository/question_repostory.dart';
